9211b32a0d804095743897740f00fe49c92271b5
Mambo Bryan <mambobryan@gmail.com>
2022-09-08 14:16:22 +0300

    adds todos and updates gitignore

--- app/src/main/java/com/example/gmailcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/gmailcloneapp/MainActivity.kt
@@ -12,7 +12,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
 
-
+        // TODO: set content view with viewbinding
         setContentView(R.layout.activity_main)
+
+        // TODO: set navigation component
+
+        // TODO: add 5 fragments to the app (1. MailsFragment  2. MailFragment  3. MeetFragment 4.SettingsFragment 5.AuthFragment)
+
+        // TODO: set AuthFragment as start destination
     }
 }
