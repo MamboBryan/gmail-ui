9f840b6779c5a6ff441f30a95afbb4101568321e
https://github.com/ngugikubai/mail-ui.git <kubaibrian@gmail.com>
2022-09-15 21:42:49 +0300

    Updated Navigation Component

---
+++ app/src/main/java/com/example/gmailcloneapp/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.gmailcloneapp.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
